65a3d95098c34f09bfa901701fc69d3403d3677a
Graham Barber <grahammbarber@gmail.com>
2019-09-25 22:38:55 -0700

    cleanup

--- src/components/profile.js
+++
@@ -1,9 +0,0 @@
-/** @jsx jsx */
-import { jsx } from '@emotion/core'
-import { Image } from '@rebass/emotion'
-
-import profile from '../images/profile.jpg'
-
-const ProfilePicture = () => <Image width={300} src={profile} borderRadius="50%" />
-
-export default ProfilePicture
